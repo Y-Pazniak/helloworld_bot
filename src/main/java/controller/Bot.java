2e2e731e3e0f6bd6a8a886af1b9b0dd49787eba1
Y-Pazniak <yauhenij@gmail.com>
2023-09-19 21:09:21 +0300

    Maksim's changes added. The code is better.

--- src/main/java/controller/Bot.java
+++ src/main/java/controller/Bot.java
@@ -6,13 +6,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import view.KeyboardMarkup;
 
 public class Bot extends TelegramLongPollingBot {
-    public void serve() {
-    }
-
     @Override
     public String getBotUsername() {
         return "helloworld_eugene_bot";
@@ -25,23 +24,28 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        SendMessage outMess = new SendMessage();
-        sendKeyboardButtons(outMess, update.getMessage());
-    }
+        KeyboardMarkup keyboard = new KeyboardMarkup();
+        if (update.hasMessage()) {
+            if (update.getMessage().hasText()) {
+                if ("/start".equals(update.getMessage().getText())) {
+                    try {
+                        execute(keyboard.sendInlineKeyBoardMessage(update.getMessage().getChatId()));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else if (update.hasCallbackQuery()) {
+            try {
+                SendMessage localMessage = new SendMessage();
+                localMessage.setChatId(update.getCallbackQuery().getMessage().getChatId().toString());
+                localMessage.setText(RandomPhraseSingleton.getInstance().getPhrase());
+                execute(localMessage);
+                //execute(sendInlineKeyBoardMessage(update.getCallbackQuery().getMessage().getChatId())); //here is the problem: you must take chatId from the update.getCallbackQuery(), NOT from the update.getMessage()
 
-    private void sendKeyboardButtons(SendMessage outMess, Message message) {
-        outMess.setChatId(message.getChatId().toString());
-        if (message.getText().equals("/start")) {
-            outMess.setText(Storage.SAY_HELLO.getPhrase());
-            outMess.setReplyMarkup(new KeyboardMarkup().getInstance());
-        } else {
-            outMess.setText(RandomPhraseSingleton.RandomPhraseSingletonHolder.getPhrase());
-        }
-
-        try {
-            execute(outMess);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/view/KeyboardMarkup.java
+++ src/main/java/view/KeyboardMarkup.java
@@ -1,5 +1,6 @@
 package view;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
@@ -7,18 +8,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class KeyboardMarkup {
-    private final InlineKeyboardMarkup keyboardWithButtons;
 
     public KeyboardMarkup() {
-        keyboardWithButtons = new InlineKeyboardMarkup();
-        initKeyboard();
     }
 
-    public InlineKeyboardMarkup getInstance() {
-        return this.keyboardWithButtons;
-    }
+    public SendMessage sendInlineKeyBoardMessage(long chatId) {
+        InlineKeyboardMarkup keyboardWithButtons = new InlineKeyboardMarkup();
 
-    private void initKeyboard() {
         List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
         List<InlineKeyboardButton> rowInline1 = new ArrayList<>();
 
@@ -27,7 +23,7 @@ public class KeyboardMarkup {
         inlineKeyboardButton1.setCallbackData("button 1");
 
         InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
-        inlineKeyboardButton2.setText("button 1");
+        inlineKeyboardButton2.setText("button 2");
         inlineKeyboardButton2.setCallbackData("button 2");
 
         rowInline1.add(inlineKeyboardButton1);
@@ -35,6 +31,13 @@ public class KeyboardMarkup {
 
         rowsInline.add(rowInline1);
         keyboardWithButtons.setKeyboard(rowsInline);
+
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId + "");
+        message.setText("hallo");
+        message.setReplyMarkup(keyboardWithButtons);
+
+        return message;
     }
 
 }
